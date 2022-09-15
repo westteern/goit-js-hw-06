53fd51f111e9304c303b27b6380fc5cb76660b54
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-09-16 00:37:10 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,7 @@ const inputRef = document.querySelector("input");
 const idRef = document.querySelector("#text");
 
 inputRef.addEventListener("input", changeSize);
+
 inputRef.value = Number(inputRef.getAttribute("min"));
 function changeSize() {
 	idRef.style.fontSize = `${inputRef.value}px`;
