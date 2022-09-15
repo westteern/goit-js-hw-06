56bba0d1faf801d3c3cbd139e7eed8e998917a98
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-09-16 00:32:57 +0300

    add_task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const { email, password } = event.currentTarget.elements;
+
+	if (email.value === "" || password.value === "") {
+		return alert("Please fill in all fields!");
+	}
+
+	console.log(`Login: ${email.value}, Password: ${password.value}`);
+	event.currentTarget.reset();
+}
