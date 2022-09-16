94b26693de4b2b1adfa39d421e147dfa23a7155e
Pavel Mikhaylenko <mikhaylenko.p@gmail.com>
2022-09-16 15:40:05 +0300

    add_task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const colorRef = document.querySelector(".color");
+const changeColorBtnRef = document.querySelector(".change-color");
+const bodyRef = document.querySelector("body");
+console.dir(colorRef);
+
+changeColorBtnRef.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	bodyRef.style.backgroundColor = color;
+	colorRef.textContent = color;
 }
