cf4561edbfcd89984d798b1e96c49f8a7478ba3c
Pavel Mikhaylenko <mikhaylenko.p@gmail.com>
2022-09-16 16:11:49 +0300

    in_progress

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,9 @@
+const inputRef = document.querySelector("input");
+const btnCreateRef = document.querySelector("button[data-create]");
+const btnDestroyRef = document.querySelector("button[data-destroy]");
+
+console.dir(inputRef);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
